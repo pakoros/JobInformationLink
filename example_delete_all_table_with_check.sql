242b9d58efbcb469c7e2b16498e4d9195d0856ec
pakoros <pakoros@hotmail.com>
2019-07-29 22:35:36 +0700

    Create example_delete_all_table_with_check.sql

---
+++ example_delete_all_table_with_check.sql
@@ -0,0 +1,10 @@
+IF OBJECT_ID('dbo.AspNetUserRoles', 'U') IS NOT NULL
+	DROP TABLE dbo.AspNetUserRoles;
+IF OBJECT_ID('dbo.AspNetUserClaims', 'U') IS NOT NULL
+	DROP TABLE dbo.AspNetUserClaims;
+IF OBJECT_ID('dbo.AspNetRoles', 'U') IS NOT NULL
+	DROP TABLE dbo.AspNetRoles;
+IF OBJECT_ID('dbo.AspNetUserLogins', 'U') IS NOT NULL
+	DROP TABLE dbo.AspNetUserLogins;
+IF OBJECT_ID('dbo.AspNetUsers', 'U') IS NOT NULL
+	DROP TABLE dbo.AspNetUsers;
